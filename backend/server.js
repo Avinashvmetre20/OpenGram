2535f8d76a1e22d16b0e05cb9403df1a7cc97fe1
Avinashvmetre20 <avinashvmetre20@gmail.com>
2025-07-11 00:24:13 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const router = require("./routers/userRoutes")
+const mongoDB = require("./config/db")
 dotenv.config();
 
 
@@ -7,7 +9,10 @@ const app = express();
 
 app.use(express.json());
 
+app.use("/auth",router);
+
 const PORT = process.env.PORT
 app.listen(PORT,()=>{
+    mongoDB();
     console.log(`Server is running on ${PORT}`)
 })
